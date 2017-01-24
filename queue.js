1342f74249491a7cd4cc3a368e4c044cae7d068b
Evan Paige <ewpaige@gmail.com>
2017-01-23 22:01:34 -0800

    remove log statements

--- queue.js
+++ queue.js
@@ -15,24 +15,3 @@ Queue.prototype.deQueue = function() {
 };
 
 const playlist = new Queue();
-playlist.enQueue('STRING');
-console.log(playlist);
-playlist.enQueue('NUMBER');
-console.log(playlist);
-playlist.enQueue('BOOLEAN');
-console.log(playlist);
-playlist.enQueue('OBJECT');
-console.log(playlist);
-playlist.enQueue('ARRAY');
-console.log(playlist);
-
-playlist.deQueue();
-console.log(playlist);
-playlist.deQueue();
-console.log(playlist);
-playlist.deQueue();
-console.log(playlist);
-playlist.deQueue();
-console.log(playlist);
-playlist.deQueue();
-console.log(playlist);
