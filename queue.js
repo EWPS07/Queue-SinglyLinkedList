a9ca7dd3fd91e754c0f1596c201ce6b437420ed3
Evan Paige <ewpaige@gmail.com>
2017-01-24 13:31:55 -0800

    seperate tests into seperate file

--- queue.js
+++ queue.js
@@ -2,6 +2,8 @@
 
 const Queue = require('./singly-linked-list.js');
 
+module.exports = Queue;
+
 Queue.prototype.enQueue = function(node) {
   this.append(node);
   return;
@@ -13,35 +15,3 @@ Queue.prototype.deQueue = function() {
   node.next = null;
   return node.val;
 };
-
-let playlist = new Queue();
-playlist.enQueue('good');
-playlist.enQueue('night');
-playlist.enQueue('moon');
-playlist.enQueue('good');
-playlist.enQueue('night');
-playlist.enQueue('great');
-playlist.enQueue('green');
-playlist.enQueue('room');
-
-console.assert(playlist.deQueue() == 'good', 'Dequeued unexpected value');
-console.assert(playlist.deQueue() == 'night', 'Dequeued unexpected value');
-console.assert(playlist.deQueue() == 'moon', 'Dequeued unexpected value');
-
-playlist.enQueue('by');
-playlist.enQueue('margret');
-playlist.enQueue('wise');
-playlist.enQueue('brown');
-
-console.assert(playlist.deQueue() == 'good', 'Dequeued unexpected value');
-console.assert(playlist.deQueue() == 'night', 'Dequeued unexpected value');
-console.assert(playlist.deQueue() == 'great', 'Dequeued unexpected value');
-console.assert(playlist.deQueue() == 'green', 'Dequeued unexpected value');
-console.assert(playlist.deQueue() == 'room', 'Dequeued unexpected value');
-
-console.assert(playlist.deQueue() == 'by', 'Dequeued unexpected value');
-console.assert(playlist.deQueue() == 'margret', 'Dequeued unexpected value');
-console.assert(playlist.deQueue() == 'wise', 'Dequeued unexpected value');
-console.assert(playlist.deQueue() == 'brown', 'Dequeued unexpected value');
-
-console.log('Hooray! All tests pass!');
